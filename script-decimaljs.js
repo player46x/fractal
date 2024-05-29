cb03048d7638161fdfff14dc3be2ecce6f877062
player46x <117786464+player46x@users.noreply.github.com>
2024-05-29 05:07:47 +0000

    so so so so much better (but not enough)

--- script-decimaljs.js
+++ script-decimaljs.js
@@ -1,4 +1,17 @@
-//Grid Checks + Skip Checksx5 + DecimalLight, 50 precision, 50x50, 255 iterations - 2.165
+// OLD AND BAD DO NOT USE
+
+
+
+
+
+
+
+//Unoptimized, 50 precision, 50x50, 255 iterations - 0.985
+//Grid Checks, 50 precision, 50x50, 255 iterations - 1.221
+//Grid Checks + Skip Checks, 50 precision, 50x50, 255 iterations - 1.426
+//Grid Checks + Skip Checksx2, 50 precision, 50x50, 255 iterations - 1.495
+//Grid Checks + Skip Checksx3, 50 precision, 50x50, 255 iterations - 1.505
+//Grid Checks + Skip Checksx5, 50 precision, 50x50, 255 iterations - 1.522
 Decimal.set({ precision: 50 })
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
@@ -15,21 +28,11 @@ var zoomn = new Decimal(1.4);
 var iterations = 255;
 var res = 50;
 
-var doEase=false;
-
-function valueEase(x)
-{
-    if(doEase)
-    {
-        return x**10;
-    }
-    return x;
-}
 
 //requestAnimationFrame(draw);
 function draw()
 {
-    zoom = zoomn.pow(3).div(2).mul(res / 4);
+    zoom = Decimal.pow(zoomn, 3).div(2).mul(res / 4);
 
     var ScreenArray = [];
 
@@ -147,7 +150,7 @@ function draw()
     for (let x = 0; x < res; x++) {
         for (let y = 0; y < res; y++) {
             let i = ScreenArray[x][y];
-            let color = HSVtoRGB((i%255)/50,1,1-valueEase(parseFloat(i)/parseFloat(iterations)));
+            let color = HSVtoRGB((i%255)/50,1,1-i/iterations);
             setPixel(x,y,color.r,color.g,color.b);
         }
     }
@@ -160,23 +163,23 @@ function draw()
 document.onkeydown = function(e) { 
     switch (e.key) { 
         case "d": 
-            xOffset = xOffset.add(new Decimal(1).div(zoom.div(10000)));
+            xOffset = Decimal.add(xOffset, Decimal.div(1,Decimal.div(zoom,10000)));
             break; 
         case "a": 
-            xOffset = xOffset.sub(new Decimal(1).div(zoom.div(10000)));
+            xOffset = Decimal.sub(xOffset, Decimal.div(1,Decimal.div(zoom,10000)));
             break; 
         case "w": 
-            yOffset = yOffset.add(new Decimal(1).div(zoom.div(10000)));
+            yOffset = Decimal.add(yOffset, Decimal.div(1,Decimal.div(zoom,10000)));
             break; 
         case "s": 
-            yOffset = yOffset.sub(new Decimal(1).div(zoom.div(10000)));
+            yOffset = Decimal.sub(yOffset, Decimal.div(1,Decimal.div(zoom,10000)));
 
             break; 
         case "e": 
-            zoomn = zoomn.add(zoomn.div(2));
+            zoomn = Decimal.add(zoomn, Decimal.div(zoomn,2));
             break; 
         case "q": 
-            zoomn = zoomn.sub(zoomn.div(4));
+            zoomn = Decimal.sub(zoomn, Decimal.div(zoomn,4));
             break; 
         case "1": 
             res = 50;
@@ -212,61 +215,57 @@ document.onkeydown = function(e) {
 }; 
 function MandelbrotFunction(xi, yi)
 {
-    let y = new Decimal(xi).div(zoom);
-    let x = new Decimal(yi).div(zoom);
+    let y = Decimal.div(xi,zoom);
+    let x = Decimal.div(yi,zoom);
 
-    x = x.add(xOffset.div(10000));
-    y = y.sub(yOffset.div(10000));
+    x = Decimal.add(x, Decimal.div(xOffset,10000));
+    y = Decimal.sub(y, Decimal.div(yOffset,10000));
 
     var zx = new Decimal(0);
     var zy = new Decimal(0);
-    var cx = new Decimal(x);
-    var cy = new Decimal(y);
+    var cx= new Decimal(x);
+    var cy= new Decimal(y);
 
     for (let i = 0; i < iterations; i++)
     {
-
-        i++
-        let xt = zx.mul(zy);
-        zx = zx.mul(zx).sub(zy.mul(zy)).add(cx);
-        zy = xt.mul(2).add(cy);
+        i++;
+        xt = Decimal.mul(zx,zy);
+        zx = Decimal.add(Decimal.sub(Decimal.mul(zx,zx), Decimal.mul(zy,zy)), cx);
+        zy = Decimal.add(Decimal.mul(2,xt), cy);
         let oldoldoldoldzx = new Decimal(zx);
         let oldoldoldoldzy = new Decimal(zy);
-
-        i++
-        xt = zx.mul(zy);
-        zx = zx.mul(zx).sub(zy.mul(zy)).add(cx);
-        zy = xt.mul(2).add(cy);
+        i++;
+        xt = Decimal.mul(zx,zy);
+        zx = Decimal.add(Decimal.sub(Decimal.mul(zx,zx), Decimal.mul(zy,zy)), cx);
+        zy = Decimal.add(Decimal.mul(2,xt), cy);
         let oldoldoldzx = new Decimal(zx);
         let oldoldoldzy = new Decimal(zy);
-
-        i++
-        xt = zx.mul(zy);
-        zx = zx.mul(zx).sub(zy.mul(zy)).add(cx);
-        zy = xt.mul(2).add(cy);
+        i++;
+        xt = Decimal.mul(zx,zy);
+        zx = Decimal.add(Decimal.sub(Decimal.mul(zx,zx), Decimal.mul(zy,zy)), cx);
+        zy = Decimal.add(Decimal.mul(2,xt), cy);
         let oldoldzx = new Decimal(zx);
         let oldoldzy = new Decimal(zy);
-
-        i++
-        xt = zx.mul(zy);
-        zx = zx.mul(zx).sub(zy.mul(zy)).add(cx);
-        zy = xt.mul(2).add(cy);
+        i++;
+        xt = Decimal.mul(zx,zy);
+        zx = Decimal.add(Decimal.sub(Decimal.mul(zx,zx), Decimal.mul(zy,zy)), cx);
+        zy = Decimal.add(Decimal.mul(2,xt), cy);
         let oldzx = new Decimal(zx);
         let oldzy = new Decimal(zy);
 
-        xt = zx.mul(zy);
-        zx = zx.mul(zx).sub(zy.mul(zy)).add(cx);
-        zy = xt.mul(2).add(cy);
-
-        if (zx.mul(zx).add(zy.mul(zy)).greaterThan(4))
+        xt = Decimal.mul(zx,zy);
+        zx = Decimal.add(Decimal.sub(Decimal.mul(zx,zx), Decimal.mul(zy,zy)), cx);
+        zy = Decimal.add(Decimal.mul(2,xt), cy);
+        
+        if (Decimal.add(Decimal.mul(zx,zx),Decimal.mul(zy,zy)).greaterThan(4))
         {
-            if (oldzx.mul(oldzx).add(oldzy.mul(oldzy)).greaterThan(4))
+            if (Decimal.add(Decimal.mul(oldzx,oldzx),Decimal.mul(oldzy,oldzy)).greaterThan(4))
             {
-                if (oldoldzx.mul(oldoldzx).add(oldoldzy.mul(oldoldzy)).greaterThan(4))
+                if (Decimal.add(Decimal.mul(oldoldzx,oldoldzx),Decimal.mul(oldoldzy,oldoldzy)).greaterThan(4))
                 {
-                    if (oldoldoldzx.mul(oldoldoldzx).add(oldoldoldzy.mul(oldoldoldzy)).greaterThan(4))
+                    if (Decimal.add(Decimal.mul(oldoldoldzx,oldoldoldzx),Decimal.mul(oldoldoldzy,oldoldoldzy)).greaterThan(4))
                     {
-                        if (oldoldoldoldzx.mul(oldoldoldoldzx).add(oldoldoldoldzy.mul(oldoldoldoldzy)).greaterThan(4))
+                        if (Decimal.add(Decimal.mul(oldoldoldoldzx,oldoldoldoldzx),Decimal.mul(oldoldoldoldzy,oldoldoldoldzy)).greaterThan(4))
                         {
                             return i-4;
                         }
